47e751093bdc9c099accee516b36618595b44d20
MRR1L0 <murilow.cc@gmail.com>
2023-02-28 22:16:12 -0300

    produtosDAO

--- src/model/DAO/ItensCompra.java
+++ src/model/DAO/ItensCompra.java
@@ -4,7 +4,12 @@
  * and open the template in the editor.
  */
 package model.DAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,6 +19,21 @@ public class ItensCompra implements InterfaceDAO<model.bo.ItensCompra> {
 
     @Override
     public model.bo.ItensCompra create(model.bo.ItensCompra t) {
+        
+                Connection conexao = ConnectionFactory.getConnection();
+        
+        var sqlExecutar = "INSERT INTO pagar "+t.sqlConection()+" values(?,?,?,?,?,?,?,?,?,?,?)";
+  
+        try {
+            
+            PreparedStatement pstm = conexao.prepareStatement(sqlExecutar);
+            pstm.setString(0, t.getDescricao());
+            pstm.setString(1, String.valueOf(t.getValorCompra()));
+            pstm.executeUpdate();
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
