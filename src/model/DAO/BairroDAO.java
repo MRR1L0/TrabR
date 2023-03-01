914b3c106ff063ae893412f092157a335be21e7e
MRR1L0 <murilow.cc@gmail.com>
2023-02-28 21:15:50 -0300

    DAO

--- src/model/DAO/BairroDAO.java
+++ src/model/DAO/BairroDAO.java
@@ -9,8 +9,6 @@ import java.sql.Connection;
 import model.bo.Bairro;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  *
  * @author aluno
